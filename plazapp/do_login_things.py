183c28a5002c50b5fa9672deeacb811ae5df99e4
Alex Harding <cpsaicontact@gmail.com>
2018-01-20 23:55:22 +0000

    started adding support for login

---
+++ plazapp/do_login_things.py
@@ -0,0 +1,11 @@
+import uuid
+import hashlib
+
+
+def hash_password(username, password):
+	hashboy = hashlib.new('sha512')
+	salt = username[::-1]
+	combined = salt + password
+	hashboy.update(combined.encode('utf-8'))
+	password_hash = hashboy.hexdigest()
+	return (salt, password_hash)

cbbb4eab30a02b6abc3788391e145f2619b6f8d5
Jacob Sacdalan <jsacdalan@ltu.edu>
2018-01-20 22:34:27 +0000

    conk

--- plazapp/model.py
+++ plazapp/model.py
@@ -17,3 +17,10 @@ def close_db(error):
 	if hasattr(flask.g, 'sqlite_db'):
 		flask.g.sqlite_db.commit()
 		flask.g.sqlite_db.close()
+
+
+def get_comments():
+	cursor = get_db()
+	cursor.execute("SELECT username FROM users WHERE userid = 1")
+	print(cursor.fetchall())
+	

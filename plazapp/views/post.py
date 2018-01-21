7e4f3b96fd5b7a9c181a8698287d31b35809c395
Alex Harding <cpsaicontact@gmail.com>
2018-01-21 02:43:12 +0000

    working on rednering posts

--- plazapp/views/post.py
+++ plazapp/views/post.py
@@ -2,10 +2,15 @@ import flask
 from flask import request
 from flask import make_response
 import plazapp
+from plazapp.model import get_comments, get_title, get_posttext
 
 @plazapp.app.route('/p/<post_id>')
 def show_post(post_id):
 	payload = {}
+	get_comments(post_id)
+	payload['posttitle'] = get_title(post_id)
+	payload['posttext'] = get_posttext(post_id)
+
 	response = make_response(flask.render_template("post.html", **payload))
 
 	return response

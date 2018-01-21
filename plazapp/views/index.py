92a2905eb092d2d9d7b42bb33c9d5ae25a92dc13
Connor <stabnic4@msu.edu>
2018-01-21 02:43:57 +0000

    trying to get test to work

--- plazapp/views/index.py
+++ plazapp/views/index.py
@@ -8,12 +8,19 @@ from plazapp.model import get_posts
 def show_index():
         log_status = request.cookies.get('loggedin')
         payload = {}
-        post1 = {'postid': 1, 'title':'conk', 'commentcount': 10}
+        #post1 = {'postid': 1, 'title':'conk', 'commentcount': 10}
         post2 = {'postid': 2, 'title':'worldstar', 'commentcount': 21}
         post3 = {'postid': 3, 'title':'ferda', 'commentcount': 3771}
 
         posts = get_posts(10)
+        
+        #second is title
+        #first is the worst (post id)
 
+        for i in posts:
+            print(i)
+
+        post1 = {'postid':1, 'title':'hi', 'commentcount':123}
 
         payload['posts'] = [post1, post2, post3]
         if log_status == "yes":

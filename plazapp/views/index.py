220ed5f2c73d9de451a26cdc9afaf1cf5019b8c9
Connor <stabnic4@msu.edu>
2018-01-21 01:48:38 +0000

    index file fixed

--- plazapp/views/index.py
+++ plazapp/views/index.py
@@ -6,16 +6,21 @@ from plazapp.model import *
 
 @plazapp.app.route('/')
 def show_index():
-	payload = {}
-	post1 = {'postid': 1, 'title':'conk', 'commentcount': 10}
-	post2 = {'postid': 2, 'title':'worldstar', 'commentcount': 21}
-	post3 = {'postid': 3, 'title':'ferda', 'commentcount': 3771}
-	payload['posts'] = [post1, post2, post3]
-	response = make_response(flask.render_template("index.html", **payload))
-
-	get_comments()
-	get_commentor()
-	get_username()
-	get_posts()
-	get_poster()
-	return response
+        log_status = request.cookies.get('loggedin')
+        payload = {}
+        post1 = {'postid': 1, 'title':'conk', 'commentcount': 10}
+        post2 = {'postid': 2, 'title':'worldstar', 'commentcount': 21}
+        post3 = {'postid': 3, 'title':'ferda', 'commentcount': 3771}
+        payload['posts'] = [post1, post2, post3]
+        if log_status == "yes":
+                 payload['loggedin'] = True
+        else:
+                 payload['loggedin'] = True
+        
+        response = make_response(flask.render_template("index.html", **payload))
+        get_comments()
+        get_commentor()
+        get_username()
+        get_posts()
+        get_poster()
+        return response

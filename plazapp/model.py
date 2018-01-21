f0a85587d44d0ba285ff780732fc8ab610917989
Alex Harding <cpsaicontact@gmail.com>
2018-01-21 00:22:21 +0000

    tested model

--- plazapp/model.py
+++ plazapp/model.py
@@ -5,12 +5,11 @@ import plazapp
 #this will connect to our database
 def get_db():
 	if not hasattr(flask.g, 'sqlite_db'):
-		flask.g.sqlite_db = sqlite.connect(plaza.app.config['DATABASE_FILENAME'])
+		flask.g.sqlite_db = sqlite3.connect(plazapp.app.config['DATABASE_FILENAME'])
 		# BACKWARDS COMPAT FOR SQLITE3
 		flask.g.sqlite_db('PRAGMA foreign_keys = ON')
 
 	return flask.g.sqlite_db
-
 
 @plazapp.app.teardown_appcontext
 def close_db(error):
@@ -19,28 +18,28 @@ def close_db(error):
 		flask.g.sqlite_db.close()
 
 
-def get_comments(postid):
-	cursor = get_db()
-	cursor.execute("SELECT commentid FROM comments WHERE commentid = %d" % postid)
+def get_comments():
+	cursor = get_db().cursor()
+	cursor.execute("SELECT * FROM comments")
 	print(cursor.fetchall())
 	
 def get_commentor():
-	cursor = get_db()
-	cursor.exeute("SELECT userid FROM comments WHERE userid = 1")
+	cursor = get_db().cursor()
+	cursor.execute("SELECT userid FROM comments WHERE userid = 1")
 	print(cursor.fetchall())
 	
 def get_username():
-	cursor = get_db()
+	cursor = get_db().cursor()
 	cursor.execute("SELECT username FROM users WHERE userid = 1")
-	print(cursor.ferchall())
+	print(cursor.fetchall())
 	
 def get_posts():
-	cursor = get_db()
+	cursor = get_db().cursor()
 	cursor.execute("SELECT postid FROM posts WHERE postid = 1")
 	print(cursor.fetchall())
 	
 def get_poster():
-	cursor = get_db()
+	cursor = get_db().cursor()
 	cursor.execute("SELECT userid FROM comments WHERE commentid = 1")
 	print(cursor.fetchall())
 	
